664f48eeeb3b54b104158fa188ec3d89b5bed523
Ayushjaisgit <ayushcodez@gmail.com>
2022-10-20 02:46:27 +0530

    update topplay.jsx

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -10,12 +10,23 @@ import {useGetTopChartsQuery} from '../redux/services/shazamCore';
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
- 
+
+const TopChartCard =( song , i )=>( 
+  <div className="w-full mb-2 flex flex-row items-center  hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer">
+    {song.title}
+  </div>
+)
+
+
 const TopPlay = () => { 
   const dispatch = useDispatch();
   const {activeSong , isPlaying} = useSelector((state)=>state.player);
   const {data} = useGetTopChartsQuery();
+
   const divRef = useRef(null);
+  useEffect(()=>{
+    divRef.current.scrollIntoView({ behavior: 'smooth'})
+  })
 
   const topPlays = data?.slice(0,5);
 
@@ -29,12 +40,37 @@ const TopPlay = () => {
   } ;
 
   return(
-    <div ref={divRef} >
-
+    <div ref={divRef} className='xl:ml-6 ml-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col'>
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center ">
+          <h2 className="text-white font-bold text-2xl"> Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base">See More</p>
+          </Link>
+        </div>
+          <div className="mt-4 flex flex-col gap-1">
+            {topPlays?.map((song, i ) => (
+              <TopChartCard 
+              key={song.key}
+              song={song}
+              i={i}
+              />
+            ))}
+          </div>
+      </div>
+      <div className="w-full flex flex-col mt-8">
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center ">
+          <h2 className="text-white font-bold text-2xl"> Top Artists</h2>
+          <Link to="/top-artists">
+            <p className="text-gray-300 text-base">See More</p>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
-
-
-  };
+    </div>
+  );
+};
+          
 
 export default TopPlay;
